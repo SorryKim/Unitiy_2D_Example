1f8d76591f4aa74db365f7e3ad65f6b951b8cc7c
kim <06zzkimzz06@gmail.com>
2022-07-12 17:55:35 +0900

    2022-07-12
    타일맵을 사용하여 플랫폼 구현

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -64,11 +64,11 @@ public class PlayerMove : MonoBehaviour
         // Landing Platform
         if (rigid.velocity.y < 0)
         {
-            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            // Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
             if (rayHit.collider != null)
             {
-                if (rayHit.distance < 0.5f)
+                if (rayHit.distance < 0.4f)
                 {
                     anim.SetBool("isJumping", false);
                 }
